fd40bdb09c863aeabdbb72afd79d1d381c59499d
Yasmin <yasmin.jobs33@gmail.com>
2026-01-20 10:42:50 -0300

    variavéis e reset do css do Pomodoro

--- Pomodoro/src/App.tsx
+++ Pomodoro/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { TimerIcon } from 'lucide-react';
 import './App.css'
+import { Heading } from './components/Heading';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  return(
+    < >
+    <Heading>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-3xl font-bold underline">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button>P
+        <TimerIcon/>
+      </button>
+    </Heading>
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, molestiae debitis. Illum magnam optio pariatur quas, mollitia velit saepe laborum omnis libero? Ducimus temporibus distinctio totam qui, odio cumque animi.
+    </div>
     </>
-  )
+  );
 }
 
-export default App
---
+++ Pomodoro/src/components/Heading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../App.css";
+import styles from "./styles/Heading.module.css";
+
+
+export function Heading(props){
+    return <h1 className = {styles.heading}></h1>
+}
